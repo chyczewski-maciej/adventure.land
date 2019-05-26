80af54d7f02137c822a716c1d2a86d36e0f57542
Chyczewski Maciej <chyczewski.maciej@gmail.com>
2019-05-26 22:24:47 +0200

    Small fixes to heal_friends function

--- heal_friends.js
+++ heal_friends.js
@@ -1,9 +1,9 @@
-function heal_friends(friends) {
+function heal_friends(friends, predicate) {
     var target = get_target();
     friends
       .map(get_player)
       .filter(player => player)
-      .filter(in_range)
+      .filter(predicate)
       .filter(player => (player.hp < (player.max_hp * 0.5)) || (player.hp < player.max_hp - (character.int * 5)))
       .sort(player => player.hp / player.max_hp)
       .slice(0, 1) // take 1
