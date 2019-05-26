d4a981d7ae57378d150b410864a66e8bdf980938
Chyczewski Maciej <chyczewski.maciej@gmail.com>
2019-05-26 16:47:02 +0200

    Fixed a bug in load_external script

--- load_external_script.js
+++ load_external_script.js
@@ -1,9 +1,7 @@
-function load_external_script(url, name) {
+function load_external_script(url) {
   $.ajaxSetup({async: false});
   $.get(url).done(eval).fail(() => {
-    log("Failed to load a script: " + name + ". Retrying...");
-    setTimeout(() => load_external_script(name), 500);
+    log("Failed to load a script. Retrying...");
+    setTimeout(() => load_external_script(url), 500);
   });
 }
-
-load_external_script('https://raw.githubusercontent.com/chyczewski-maciej/adventure.land/master/load_all_scripts.js');
