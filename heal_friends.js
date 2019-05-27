de181f5a350456cf45f0451ddfe7279b0a830f60
Chyczewski Maciej <chyczewski.maciej@gmail.com>
2019-05-27 23:13:19 +0200

    Removed dead players from heal_friends list

--- heal_friends.js
+++ heal_friends.js
@@ -3,7 +3,7 @@ function heal_friends(friends, predicate) {
 
   friends
     .map(get_player)
-    .filter(player => player)
+    .filter(player => player && !player.rip)
     .filter(player => (!predicate) || predicate(player))
     .filter(player => (player.hp < (player.max_hp * 0.5)) || (player.hp < player.max_hp - (character.int * 5)))
     .sort(player => player.hp / player.max_hp)
